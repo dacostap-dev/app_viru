b07b10a70c3193fa6827535b40b485e286afbfb3
Daniel Acosta <d4ni3L_15@hotmail.com>
2019-05-08 10:28:07 -0500

    solución a blank screen on build

--- src/main.js
+++ src/main.js
@@ -13,5 +13,9 @@ Vue.use(BootstrapVue);
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted() {
+    // Prevent blank screen in Electron builds
+    this.$router.push('/')
+  }
 }).$mount('#app')
